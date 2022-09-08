604bd26899e40d4e24832e1ae1cb190515d61449
Qamar Abbas <reply.qamar@gmail.com>
2022-09-08 16:34:43 +0500

    Changed shop-data.json to js file and uploaded data/categories to firestore

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,12 +1,17 @@
-import { createContext, useState } from 'react';
-import PRODUCTS from '../shop-data.json';
+import { createContext, useState, useEffect } from 'react';
+// import SHOP_DATA from '../shop-data.js';
+import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils.js';
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+  // it should only load once , we have created categories by this way
+  // useEffect(() => {
+  //   addCollectionAndDocuments('categories', SHOP_DATA);
+  // }, []);
   const value = { products };
   return (
     <ProductsContext.Provider value={value}>
