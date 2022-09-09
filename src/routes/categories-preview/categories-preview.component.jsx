1c31fb0c9dce8978b7b12a56bc37ede8a64c3cc3
Qamar Abbas <reply.qamar@gmail.com>
2022-09-09 19:10:19 +0500

    Nested routes for Shop Route and useparams state

---
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,17 @@
+import { Fragment, useContext } from 'react';
+import { CategoriesContext } from '../../contexts/categories.context';
+import CategoryPreview from '../../components/category-preview/category-preview.component';
+const CategoriesPreview = () => {
+  const { categoriesMap } = useContext(CategoriesContext);
+  return (
+    <Fragment>
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview products={products} key={title} title={title} />
+        );
+      })}
+    </Fragment>
+  );
+};
+export default CategoriesPreview;
