3597df8a59add612a4f194f54051996a4513c671
Qamar Abbas <reply.qamar@gmail.com>
2022-09-10 03:21:29 +0500

    Created _redirects for netlify

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import {
   DirectoryItemContainer,
   BackgroundImage,
@@ -5,9 +6,11 @@ import {
 } from './directory-item.styles';
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+  const onNavigateHandler = () => navigate(route);
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage
         style={{ backgroundImage: `url(${imageUrl})` }}
       ></BackgroundImage>
